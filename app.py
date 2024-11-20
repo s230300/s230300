df429162b261e00a2e2e4538a67d27b0a4d4138f
s230300 <gimpo.s230300@ggh.goe.go.kr>
2024-11-20 10:18:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,17 +22,20 @@ with col2:
       st.subheader('데이터시각화1')
       st.image('다운로드.png' )
 with col3:
+      st.subheader('데이터시각화2')
+      st.image('_____1_.png')
       
 
 # 4. 모델 활용
 st.subheader('모델 활용')
 st.write('**** 다음을 입력하세요.. 인공지능이 당신의 알코올 소비량을 알려드립니다! ')
 
-a = st.number_input(' 나이 입력 ', value=1)      
+a = st.number_input(' 나이 입력 ', value=1) 
+b = st.selctbox(' 성별 입력'('남성:'M', 여성:'F'', ['M,F'])
 
                                                           
 
 if st.button('알코올 소비량 예측'):           
-        input_data = [[a]]     
+        input_data = [[a,b]]     
         p = model.predict(input_data)         
         st.write('인공지능의 예측 알코올 소비량은', p)
